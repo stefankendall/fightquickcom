cc9f37bb662ebb2cc6bb3d34fb4ed05499e289aa
Stefan Kendall <stefankendall@gmail.com>
2013-11-02 19:53:42 -0400

    Now renders all data. Ember-data is slow as hell.

--- public/js/models/pokemon.js
+++ public/js/models/pokemon.js
@@ -2,8 +2,3 @@ App.Pokemon = DS.Model.extend({
     name: DS.attr('string'),
     types: DS.attr('array')
 });
-
-App.Pokemon.FIXTURES = [
-    {id: 1, name: "Charizard", types: ['fire', 'flying']},
-    {id: 2, name: "Charizard", types: ['fire', 'flying']}
-];
