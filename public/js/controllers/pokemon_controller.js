e1a7893d5fafd4b0c79d5fc0e5e21c2a8eaa3424
Stefan Kendall <stefankendall@gmail.com>
2013-11-02 19:37:16 -0400

    Added lodash. Added type effectiveness calculation.

--- public/js/controllers/pokemon_controller.js
+++ public/js/controllers/pokemon_controller.js
@@ -1,6 +1,7 @@
 App.PokemonController = Ember.ObjectController.extend({
     superEffectiveTypes: function (a) {
         var effectiveness = this._typeEffectiveness(this.get('model').get('types'));
+        console.log(effectiveness);
         return effectiveness.filter(function (value) {
             return value.value > 1;
         });
@@ -11,10 +12,19 @@ App.PokemonController = Ember.ObjectController.extend({
             return value.value < 1;
         });
     }.property('model.types'),
-    _typeEffectiveness: function (types) {
-        return [
-            {name: 'fire', value: 0.25},
-            {name: 'water', value: 2}
-        ];
+    _typeEffectiveness: function (defendingTypes) {
+        var effectivenesses = Ember.keys(App.TypeChart).map(function (attackingType) {
+            var effectiveness = 1;
+            defendingTypes.forEach(function (defendingType) {
+                effectiveness *= App.TypeChart[attackingType][defendingType];
+            });
+            return {
+                name: attackingType,
+                value: effectiveness
+            };
+        });
+        return _.sortBy(effectivenesses, function (object) {
+            return object.name
+        });
     }
 });
