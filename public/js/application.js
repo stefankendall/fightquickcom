6d07be5d5c2540b0def1e28578d3f0b3363127e2
Stefan Kendall <stefankendall@gmail.com>
2013-11-02 20:05:51 -0400

    Still fast.

--- public/js/application.js
+++ public/js/application.js
@@ -6,13 +6,3 @@ App.ApplicationController = Em.Controller.extend({
         this.items = window.FIXTURES;
     }
 });
-
-Em.Handlebars.registerBoundHelper('eachPokemon', function (items) {
-    return (
-        new Handlebars.SafeString(
-            items.map(function (i) {
-                return '<span>' + JSON.stringify(i) + '</span>';
-            })
-        )
-        );
-});
